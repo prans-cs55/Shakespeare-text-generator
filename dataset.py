448d77ac813e5e5bfbd627b9d89061ed3c308954
Venkata Sai Pranavi <pranusarala@gmail.com>
2025-08-01 22:12:49 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,4 @@
 filepath=tf.keras.utils.get_file('shake.txt','https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+text=open(filepath,'rb').read().decode(encoding='utf-8')
+text=text[300000:800000]
+characters=sorted(set(text))
